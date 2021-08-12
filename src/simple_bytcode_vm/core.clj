9084673387bfff95285d3f3731b227e7ed9ea521
Divyansh Prakash <divyansh.prakash@yahoo.com>
2021-08-12 15:35:45 +0530

    Refactor: move REPL to separate ns

--- src/simple_bytcode_vm/core.clj
+++ src/simple_bytcode_vm/core.clj
@@ -1,28 +1,8 @@
 (ns simple-bytcode-vm.core
   (:gen-class)
-  (:refer-clojure :exclude [eval])
   (:require [clojure.edn :as edn]
-            [simple-bytcode-vm.compiler :as compiler]
             [simple-bytcode-vm.interpreter :as interpreter]
-            [simple-bytcode-vm.env :as env]))
-
-(defn eval
-  ([exp]
-   (eval exp (env/base-env)))
-  ([exp env]
-   (try
-     (interpreter/eval (compiler/compile exp) env)
-     (catch Throwable e
-       (.getMessage e)))))
-
-(defn repl []
-  (let [env (env/base-env)]
-    (while true
-      (print "> ")
-      (flush)
-      (-> (edn/read)
-          (eval env)
-          println))))
+            [simple-bytcode-vm.repl :as repl]))
 
 (defn -main
   [& [edn-bytecode-filename]]
@@ -31,6 +11,6 @@
       (let [text (slurp edn-bytecode-filename)
             code (edn/read-string text)]
         (interpreter/eval code))
-      (repl))
+      (repl/repl))
     (catch Throwable e
       (println (.getMessage e)))))
---
+++ src/simple_bytcode_vm/repl.clj
@@ -0,0 +1,25 @@
+(ns simple-bytcode-vm.repl
+  (:gen-class)
+  (:refer-clojure :exclude [eval])
+  (:require [clojure.edn :as edn]
+            [simple-bytcode-vm.compiler :as compiler]
+            [simple-bytcode-vm.env :as env]
+            [simple-bytcode-vm.interpreter :as interpreter]))
+
+(defn eval
+  ([exp]
+   (eval exp (env/base-env)))
+  ([exp env]
+   (try
+     (interpreter/eval (compiler/compile exp) env)
+     (catch Throwable e
+       (.getMessage e)))))
+
+(defn repl []
+  (let [env (env/base-env)]
+    (while true
+      (print "> ")
+      (flush)
+      (-> (edn/read)
+          (eval env)
+          println))))
