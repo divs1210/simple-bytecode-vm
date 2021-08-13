6c2533e3a6608465219e9a1d34a78eca9c37cea9
Divyansh Prakash <divyansh.prakash@yahoo.com>
2021-08-13 15:32:34 +0530

    Enhance base-env

--- src/simple_bytcode_vm/env.clj
+++ src/simple_bytcode_vm/env.clj
@@ -3,24 +3,78 @@
   (:require [clojure.string :as str]
             [simple-bytcode-vm.util :as u]))
 
+(def ^:private ^:const base-env*
+  {;; Math
+   ;; ====
+   '+ +'
+   '* *'
+   '- -
+   '/ /
+   '** #(Math/pow %1 %2)
+
+   ;; Boolean
+   ;; =======
+   '< <
+   '> >
+   '= =
+   '== ==
+   '<= <=
+   '>= >=
+   'not not
+
+   ;; IO
+   ;; ==
+   'print
+   (fn [& args]
+     (print (str/join args))
+     (flush))
+
+   'println
+   (fn [& args]
+     (apply print args)
+     (print "\n"))
+
+   ;; Collections
+   ;; ===========
+   'list list
+   'vector vector
+   'hash-map hash-map
+   'list? list?
+   'vector? vector?
+   'map? map?
+
+   ;; Seq Functions
+   ;; =============
+   'seq seq
+   'seqable? seqable?
+   'first first
+   'rest rest
+   'nth nth
+   'get-in get-in
+   'assoc-in assoc-in
+   'dissoc dissoc
+
+   ;; Special
+   ;; =======
+   'apply apply
+
+   ;; Types and coercion
+   ;; ==================
+   'type type
+   'int bigint
+   'char char
+
+   'int? integer?
+   'number? number?
+   'string? string?
+
+   ;; String
+   ;; ======
+   'str (fn [& args]
+          (str/join args))})
+
 (defn base-env []
-  (atom {'+ +'
-         '* *'
-         '- -
-         '/ /
-         '** #(Math/pow %1 %2)
-
-         '< <
-         '> >
-         '= =
-         '== ==
-         '<= <=
-         '>= >=
-         'not not
-
-         'print (fn [& args]
-                  (print (str/join args))
-                  (flush))}))
+  (atom base-env*))
 
 (defn extend
   [env bindings]
